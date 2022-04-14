8187e6bde786287d2462b1c9490451bec1dd6ef9
RevathiSriMCA <103115706+RevathiSriMCA@users.noreply.github.com>
2022-04-14 17:07:16 +0530

    Add files via upload
    14.04.2022

--- Script.js
+++ Script.js
@@ -6,9 +6,8 @@ var day31MonthArr=["January","March","May","July","August","October","December"]
 
 var d= new Date();
 var obj=getDate();
-generateCalendar();
-
-
+loadXMLDoc();
+//generateCalendar();
 
 function getDate()
 {
@@ -17,14 +16,34 @@ function getDate()
     var day=d.getDay();
     day=dayArr[day]
     var date=d.getDate();
-    h2.innerHTML=date+","+day;
-    h3.innerHTML=month;
+    //h2.innerHTML=date+","+day;
+    //h3.innerHTML=month;
     return{m: month,dy: day,dt: date,yr: d.getFullYear()}
 }
 
-function generateCalendar()
+function loadXMLDoc()
 {
     var x;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function () 
+	{
+        // Request finished and response
+		// is ready and Status is "OK"
+		if (this.readyState == 4 && this.status == 200) 
+		{
+			generateCalendar(this);
+		}
+	};
+	// Jan 2023.xml is the external xml file
+	xmlhttp.open("GET", "Jan 2023.xml", true);
+	xmlhttp.send();
+}
+function generateCalendar(xml)
+{
+    var xmlDoc = xml.responseXML;
+	var x = xmlDoc.getElementsByTagName("Panchang-Information");
+    
+    //var x;
     var days;
     if(obj.m === "February" && obj.yr %4!== 0)
     {
@@ -52,14 +71,19 @@ function generateCalendar()
         element.className="calendar_number_empty";
         document.getElementById("lc").appendChild(element);
     }
-    for(var k=1; k<=days; k++)
+    for(var k=1; k<=2; k++)
     {
         var element=document.createElement("div");
         obj.dt === k ? element.className="calendar_number calendar_number--current":(element.className="calendar_number");
         element.appendChild(document.createTextNode(k));
-        debugger;
+        var Nakshathra = x[k-1].getElementsByTagName("Information")[2].childNodes[0].nodeValue; 
+	    var Thithi = x[k-1].getElementsByTagName("Information")[3].childNodes[0].nodeValue; 
+	    var Yoga = x[k-1].getElementsByTagName("Information")[5].childNodes[0].nodeValue; 
+	    var Karana = x[k-1].getElementsByTagName("Information")[6].childNodes[0].nodeValue;
         document.getElementById("lc").appendChild(element);
-        debugger;
+        element.appendChild(document.createTextNode('\n'+ Nakshathra));
+        element.appendChild(document.createTextNode('\n'+ Thithi));
+        element.appendChild(document.createTextNode('\n'+ Yoga));
+        element.appendChild(document.createTextNode('\n'+ Karana));
     }
-    
 }
