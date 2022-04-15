fc1af0d0a77d3107ffed95ebb27114725a8c45e7
muthuvijayanelango <103093357+muthuvijayanelango@users.noreply.github.com>
2022-04-15 11:14:44 +0530

    Add files via upload
    Updates on 15/04/2022

--- Script.js
+++ Script.js
@@ -19,6 +19,7 @@ function getDate()
     //h2.innerHTML=date+","+day;
     //h3.innerHTML=month;
     return{m: month,dy: day,dt: date,yr: d.getFullYear()}
+    
 }
 
 function loadXMLDoc()
@@ -38,7 +39,6 @@ function loadXMLDoc()
 	xmlhttp.open("GET", "Jan 2023.xml", true);
 	xmlhttp.send();
 }
-
 function generateCalendar(xml)
 {
     var xmlDoc = xml.responseXML;
@@ -68,27 +68,75 @@ function generateCalendar(xml)
     
     for(var j=0; j<dayIndex; j++)
     {
-        var element=document.createElement("div");
+        var element=document.createElement("td");
         element.className="calendar_number_empty";
         document.getElementById("lc").appendChild(element);
+        document.body.appendChild(element)
     }
-    for(var k=1; k<=5; k++)
+    //var tblHead = document.createElement('thead');
+    var body = document.getElementsByTagName("body")[0];
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+    var prek = 0;
+    for(var i = 0; i < 1; i++)
     {
-        var element=document.createElement("div");
-        obj.dt === k ? element.className="calendar_number calendar_number--current":(element.className="calendar_number");
-        element.appendChild(document.createTextNode(k));
-        var Nakshathra = x[k-1].getElementsByTagName("Information")[2].childNodes[0].nodeValue; 
-	    var Thithi = x[k-1].getElementsByTagName("Information")[3].childNodes[0].nodeValue; 
-	    var Yoga = x[k-1].getElementsByTagName("Information")[5].childNodes[0].nodeValue; 
-	    var Karana = x[k-1].getElementsByTagName("Information")[6].childNodes[0].nodeValue;
+        //var row = document.createElement("tr1");
+        
+        for(var k=0; k<=days; k++)
+        {
+        //var cell = document.createElement("td");
+        var weekday = x[k].getElementsByTagName("Information")[1].childNodes[0].nodeValue;
+        var element=document.createElement("td");
+        if(prek != k && weekday === "Sunday")
+        {
+        var element=document.createElement("tr");
+        element.appendChild(document.createTextNode(""));
         document.getElementById("lc").appendChild(element);
+        var prek = k;
+        k = k-1;                             
+        }
+        
+        else
+        {
+            var element=document.createElement("td");
+            //var element=document.createElement("td");
+        //obj.dt === k ? element.className="calendar_number calendar_number--current":(element.className="calendar_number");
+        //var day = dayArr[k]
+        element.appendChild(document.createTextNode(k+1));
+        //var cellText = document.createTextNode(k+1);
+        //var dayText = document.createTextNode(day);
+        //cell.appendChild(dayText);
+        //cell.appendChild(cellText);
+        //row.appendChild(cell);
+         
+        var Nakshathra = x[k].getElementsByTagName("Information")[2].childNodes[0].nodeValue; 
+	    var Thithi = x[k].getElementsByTagName("Information")[3].childNodes[0].nodeValue; 
+	    var Yoga = x[k].getElementsByTagName("Information")[5].childNodes[0].nodeValue; 
+	    var Karana = x[k].getElementsByTagName("Information")[6].childNodes[0].nodeValue;
+        //document.getElementById("lc").appendChild(cell);
+        document.getElementById("lc").appendChild(element);
+        //document.body.appendChild(element)
+        //cell.appendChild(document.createTextNode('\n'+ Nakshathra));
+        //cell.appendChild(document.createTextNode('\n'+ Thithi));
+        //cell.appendChild(document.createTextNode('\n'+ Yoga));
+        //cell.appendChild(document.createTextNode('\n'+ Karana));
         element.appendChild(document.createTextNode('\n'+ Nakshathra));
         element.appendChild(document.createTextNode('\n'+ Thithi));
         element.appendChild(document.createTextNode('\n'+ Yoga));
         element.appendChild(document.createTextNode('\n'+ Karana));
-        //debugger;
+        //row.appendChild(cell);
+        }
         
-        //debugger;
+        }
+        //tblHead.appendChild(row)
+        //tblBody.appendChild(row);
+        //document.getElementById("table").deleteRow(0);
+        
+        
+
     }
+    //tbl.appendChild(tblBody);
+  // appends <table> into <body>
+    //body.appendChild(tbl);
     
 }
